0266e75c84955ca93e5191ead25e6a4f6e099826
Francis <lara.francis1@gmail.com>
2017-11-22 11:52:59 -0800

    Add root page; Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :users, except: %i(edit new)
+    resource :session, only: %i(new create destroy)
+  end
+
+  root to: 'root#root'
+
 end
