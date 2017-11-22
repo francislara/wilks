e4a8b4880853820c6e41a7b932ed4105630834e1
Francis <lara.francis1@gmail.com>
2017-11-22 10:47:19 -0800

    Add user model

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './frontend/wilks.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: [/\.jsx?$/],
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*'],
+    }
+};
