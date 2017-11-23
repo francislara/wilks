8545ad2aeb327fcd9d7cafd407f5ce5cb8875426
Francis <lara.francis1@gmail.com>
2017-11-23 03:44:54 -0800

    Add logo

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -38,7 +38,10 @@ class Login extends React.Component {
     return (
       <div className="session-form">
         <div className="auth-container">
-          <div className="auth-logo"><p>wilks</p></div>
+          <div className="auth-left">
+            <img src='https://i.imgur.com/Cp8ii7N.png' />
+            <p>wilks</p>
+          </div>
           <form>
             <h2>Welcome Back!</h2>
             <label>Email
